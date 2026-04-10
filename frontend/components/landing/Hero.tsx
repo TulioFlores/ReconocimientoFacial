2d89650b2c62bbeaef4afda4c916fd9c3cfcb279
Tulio <22460285@colima.tecnm.mx>
2026-04-10 14:44:57 -0600

    feat: añadir animacion a la landing page

--- frontend/components/landing/Hero.tsx
+++ frontend/components/landing/Hero.tsx
@@ -1,13 +1,25 @@
+'use client';
 import Image from 'next/image';
+import Lottie from 'lottie-react';
+import animationData from '../../public/animation/data copy.json';
+import { useRef, useEffect } from 'react';
 
 export default function Hero() {
+  const lottieRef = useRef<any>(null);
+
+  useEffect(() => {
+    if (lottieRef.current) {
+      lottieRef.current.setSpeed(0.4); // Mas despacio
+    }
+  }, []);
+
   return (
     <div className="relative min-h-screen bg-background">
       {/* Hero Section */}
-      <main className="relative h-screen flex items-center">
+      <main className="relative h-screen flex items-center overflow-hidden">
 
         {/* Contenedor de la Imagen de Fondo */}
-        <div className="absolute inset-0 z-0 overflow-hidden">
+        <div className="absolute inset-0 z-0">
           <Image
             src="/hero.png" // REEMPLAZA ESTO CON LA RUTA DE TU IMAGEN
             alt="Mujer escaneo biométrico GobID"
@@ -16,10 +28,20 @@ export default function Hero() {
             priority
           />
           {/* Polarizado general de la imagen (Overlay negro suave) */}
-          <div className="absolute inset-0 bg-black/40 z-[1]" />
+          <div className="absolute inset-0 bg-black/70 z-[1]" />
 
           {/* Gradiente oscuro sutil a la izquierda para asegurar que el texto blanco se lea bien */}
           <div className="absolute inset-0 bg-gradient-to-r from-primary/80 via-primary/30 to-transparent z-[2]" />
+
+          {/* ANIMACIÓN DE ESCÁNER: Posicionada a la derecha sobre el rostro */}
+          <div className="absolute right-0 lg:right-47 top-4/9 -translate-y-1/3 z-[3] w-[600px] h-[600px] hidden lg:block opacity-80 pointer-events-none">
+            <Lottie
+              lottieRef={lottieRef}
+              animationData={animationData}
+              loop={true}
+              className="w-full h-full"
+            />
+          </div>
         </div>
 
         {/* Contenido de Texto y Botón */}
